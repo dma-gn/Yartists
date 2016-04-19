8bcd1e1e42c9c6560663e82114d20c8b38e2eb8c
Dmitry <gnetetskii@gmail.com>
2016-04-19 16:26:33 +0500

    initialize toolbar

--- app/src/main/java/com/dma/yartists/MainActivity.java
+++ app/src/main/java/com/dma/yartists/MainActivity.java
@@ -2,16 +2,25 @@ package com.dma.yartists;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 
 public class MainActivity extends Activity {
 
     private static final int LAYOUT = R.layout.activity_main;
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
 
+        initializeToolBar();
+    }
+
+    private void initializeToolBar() {
+        toolbar =(Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.artists);
     }
 }
