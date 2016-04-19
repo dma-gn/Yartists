898f6807352f3b8fe0104e3b1ac4bc21b171b0b7
Dmitry <gnetetskii@gmail.com>
2016-04-19 16:02:18 +0500

    initialize project

---
+++ app/src/main/java/com/dma/yartists/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dma.yartists;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    private static final int LAYOUT = R.layout.activity_main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(LAYOUT);
+
+    }
+}
